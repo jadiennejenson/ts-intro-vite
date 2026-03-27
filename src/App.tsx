fb6b3a7a62b5ea58e262e1518c1003b6a603a10a
jadiennejenson <10935974@uvu.edu>
2026-03-26 20:57:55 -0600

    1

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,25 @@ import ProjectsPage from './pages/ProjectsPage';
 
 export default function App() {
   return (
+    <div className="flex flex-col min-h-screen">
     <Router>
-      <nav className="p-4 bg-slate-100 flex gap-4 w-full">
+      <nav className="p-4 bg-slate-100 flex gap-4 w-full shrink-0">
         <div className="max-w-5xl mx-auto flex gap-4 w-full justify-center"> 
-          <Link to="/">Home</Link> {/* 2. Changed /Home to / to match the route */}
+          <Link to="/">Home</Link>
           <Link to="/projects">Projects</Link>
           <Link to="/tracker">Tracker</Link>
         </div>
       </nav>
 
-      <Routes>
-        {/* 3. Render the actual Home component here */}
-        <Route path="/" element={<Home />} />
-        <Route path="/projects" element={<ProjectsPage />} />
-        <Route path="/tracker" element={<Tracker />} />
-      </Routes>
+      {/* This main tag expands to fill all remaining vertical space */}
+      <main className="flex-grow">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<ProjectsPage />} />
+          <Route path="/tracker" element={<Tracker />} />
+        </Routes>
+      </main>
     </Router>
+  </div>
   );
 }
