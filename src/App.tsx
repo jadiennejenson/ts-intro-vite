646373d815da4e48ea6dfe0096e781233ac2b981
jadiennejenson <10935974@uvu.edu>
2026-03-26 22:05:18 -0600

    save

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Home from './pages/home';
+import Home from './pages/Home';
 import Tracker from './pages/Tracker';
 import ProjectsPage from './pages/ProjectsPage';
 
